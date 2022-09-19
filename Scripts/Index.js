39e68afdcbb08e3bffdcd42d244c0ce7b0994e13
Solly <solobarine@gmail.com>
2022-09-19 22:14:35 +0100

    Add remove button

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -1,5 +1,17 @@
-const bookArray = [];
+let bookArray = [];
+const localStore = JSON.parse(localStorage.getItem('Bookstore'));
+/*if (localStore) {
+  bookArray.push(localStore)
+} else {
+  bookArray = [];
+}*/
+
+
+
+
 const bookList = document.getElementById('bookList');
+// Populate the DOM
+updateList();
 
 function updateList() {
     bookList.innerHTML = '';
@@ -10,11 +22,12 @@ function updateList() {
         const bookString = `<li class="Book">
                             <div class="bookTitle">${bookTitle}</div>
                             <div class="bookAuthor">${bookAuthor}</div>
-                            <button type="button" class="removeBook">Remove</button>
+                            <button type="button" id="${i}" class="removeBook" onclick="remBtn(${i})">Remove</button>
                         </li>
         `
-        bookList.insertAdjacentHTML('beforeend', bookString);
+      bookList.insertAdjacentHTML('beforeend', bookString);
     }
+  localStorage.setItem('Bookstore', JSON.stringify(bookArray));
 }
 
 function addBook(){
@@ -25,6 +38,8 @@ function addBook(){
     const book = {bookTitle , bookAuthor}
     bookArray.push(book);
     updateList();
+    
+  // remEvent();
     titleInputElement.value = '';
     authorInputElement.value = '';
 }
@@ -32,3 +47,12 @@ function addBook(){
 const btnSubmitBook = document.getElementById('submitBook');
 btnSubmitBook.addEventListener('click', addBook);
 
+// Remove Book
+const remBtn = (a) => {
+    // Get ID of the button
+  const id = a
+  console.log(id);
+  // Remove item from Arrey
+  bookArray.splice(a, 1)
+  updateList();
+}
