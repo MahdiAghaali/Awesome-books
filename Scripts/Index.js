af072b456e205b19c1e18614ac9a43dafbae5f99
Mahdi Aghaali <93377744+MahdiAghaali@users.noreply.github.com>
2022-09-21 10:37:43 +0430

    Fix Remove method in library class

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -1,9 +1,17 @@
 import Library from './Library.js';
-import bookArray from './bookArray.js';
+import Book from './Book.js';
 
 const Lib = new Library();
-
-Lib.updateList();
+function getBook() {
+  const titleInputElement = document.getElementById('titleInput');
+  const authorInputElement = document.getElementById('authorInput');
+  const bookTitle = titleInputElement.value;
+  const bookAuthor = authorInputElement.value;
+  titleInputElement.value = '';
+  authorInputElement.value = '';
+  const book = new Book(bookTitle, bookAuthor);
+  Lib.addBook(book);
+}
 
 const btnSubmitBook = document.getElementById('submitBook');
-btnSubmitBook.addEventListener('click', Lib.addBook);
+btnSubmitBook.addEventListener('click', getBook);
--- Scripts/Library.js
+++ Scripts/Library.js
@@ -1,73 +1,46 @@
-import Book from './Book.js';
-import bookArray from './bookArray.js';
-
 export default class Library {
-  constructor() {}
+  constructor() {
+    this.books = [];
 
-  addToLocalStorage = () => localStorage.setItem('Library', JSON.stringify(libro))
-
-  addBook() {
-    const titleInputElement = document.getElementById('titleInput');
-    const authorInputElement = document.getElementById('authorInput');
-    const bookTitle = titleInputElement.value;
-    const bookAuthor = authorInputElement.value;
-    titleInputElement.value = '';
-    authorInputElement.value = '';
-
-    const book = new Book(bookTitle, bookAuthor);
-    console.log(book)
-    const localStore = JSON.parse(localStorage.getItem('Library'));
-    console.log(localStore)
-    if (localStore) {
-    bookArray.push(...localStore);
-    } else {
-      bookArray;
+    if (localStorage.getItem('Library') !== null) {
+      const lib = JSON.parse(localStorage.getItem('Library'));
+      this.books = lib.books;
+      this.updateList();
     }
-    console.log(localStore)
-    bookArray.push(book);
-    console.log(bookArray)
-    const showBook = () => {
-    bookList.innerHTML += `<li class="Book">
-                            <div class="bookTitle">${bookTitle}</div>
-                            <div class="bookAuthor">${bookAuthor}</div>
-                            <button type="button" class="removeBook" onclick="Lib.removeBook(${bookArray.length})">Remove</button>
-                        </li>`
-    }
-    showBook()
-      localStorage.setItem('Library', JSON.stringify(bookArray))
-    //this.addToLocalStorage
+  }
 
-
+  addBook(book) {
+    this.books.push(book);
+    this.updateList();
   }
 
   removeBook(index) {
-    bookArray.splice(index, 1);
-    localStorage.setItem('Library', JSON.stringify(bookArray));
+    this.books.splice(index, 1);
+    this.updateList();
   }
 
-    updateList() {
+  updateList() {
     const bookList = document.getElementById('bookList');
-      bookList.innerHTML = '';
-    const localStore = JSON.parse(localStorage.getItem('Library'))
-    if (localStore) {
-    bookArray.push(...localStore);
-    } else {
-      bookArray;
-    }
+    bookList.innerHTML = '';
 
-    for (let i = 0; i < bookArray.length; i += 1) {
-      /* const {
-        bookTitle, bookAuthor,
-      } = this.books[i];*/
+    for (let i = 0; i < this.books.length; i += 1) {
+      const {
+        Title, Author,
+      } = this.books[i];
       const bookString = `<li class="Book">
-                            <div class="bookTitle">${bookArray[i].Title}</div>
-                            <div class="bookAuthor">${bookArray[i].Author}</div>
-                            <button type="button" class="removeBook" onclick="Lib.removeBook(${i})">Remove</button>
+                            <div class="bookTitle">${Title}</div>
+                            <div class="bookAuthor">${Author}</div>
+                            <button type="button" class="removeBook" id='${i}' >Remove</button>
                         </li>
         `;
       bookList.insertAdjacentHTML('beforeend', bookString);
     }
-    localStorage.setItem('Library', JSON.stringify(bookArray));
-  }
+    bookList.querySelectorAll('button').forEach((btn, i) => {
+      btn.addEventListener('click', () => {
+        this.removeBook(i);
+      });
+    });
 
+    localStorage.setItem('Library', JSON.stringify(this));
   }
+}
